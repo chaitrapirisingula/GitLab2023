b646e5086a139f5a80d21e1e32aaf9268fd0f8ac
kle20 <89547698+kle20@users.noreply.github.com>
2023-02-03 12:08:35 -0600

    yay

--- src/GitLab.java
+++
@@ -1,8 +0,0 @@
-
-public class GitLab {
-    
-    public static void main (String [] args) {
-        System.out.println("This is a lesson on git.");
-    }
-
-}
---
+++ src/edu/unl/raikes/git/GitLab.java
@@ -0,0 +1,10 @@
+package edu.unl.raikes.git;
+
+public class GitLab {
+
+    public static void main(String[] args) {
+        GitLabHelper helper = new GitLabHelper("Kyana", "Chaitra");
+        helper.PrintWelcomeMessage();
+    }
+
+}
