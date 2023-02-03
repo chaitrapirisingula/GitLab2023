027753907def683df76b41095294718555cf6c32
chaitrapirisingula <89547692+chaitrapirisingula@users.noreply.github.com>
2023-02-03 12:12:55 -0600

    add stuff

--- src/edu/unl/raikes/git/GitLab.java
+++ src/edu/unl/raikes/git/GitLab.java
@@ -1,10 +1,8 @@
 package edu.unl.raikes.git;
 
 public class GitLab {
-
     public static void main(String[] args) {
-        GitLabHelper helper = new GitLabHelper("Kyana", "Chaitra");
+        GitLabHelper helper = new GitLabHelper(new String[] { "person1", "person2" });
         helper.PrintWelcomeMessage();
     }
-
 }
